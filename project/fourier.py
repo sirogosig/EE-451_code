6b05bdb05d3266d2ec9d20a0032d78f25a50f1af
Elias De Smijter <elias.desmijter@student.kuleuven.be>
2023-05-26 10:04:22 +0200

    fourier now also vertical

--- project/fourier.py
+++ project/fourier.py
@@ -3,10 +3,11 @@ import cv2 as cv
 import numpy as np
 
 def fourier_descr(im, descr_to_keep):
-    im_ft = fft(im)
-    m = np.mean(im_ft, axis=0)
-    v = np.var(im_ft, axis=0)
-    return list(abs(m))[:descr_to_keep],list(v)[:descr_to_keep]
+    im_ft = fft(im, axis=0)
+    m1 = np.mean(im_ft, axis=0)
+    im_ft = fft(im, axis=1)
+    m2 = np.mean(im_ft, axis=1)
+    return [int(i) for i in list(abs(m1))[:descr_to_keep]+list(abs(m2))[:descr_to_keep]]
 
 im = cv.cvtColor(cv.imread('data_project/train2_solutions/solution_00_00.png'),cv.COLOR_BGR2GRAY)
 print(im.shape)
